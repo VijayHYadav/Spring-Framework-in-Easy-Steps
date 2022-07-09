5fb20653eeac2e8a6dcc5bb664319f16cc3557f7
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-09 16:29:19 +0530

    26.Create java bean

---
+++ springcore/src/main/java/com/bharath/spring/springcore/Employee.java
@@ -0,0 +1,19 @@
+package com.bharath.spring.springcore;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+}
