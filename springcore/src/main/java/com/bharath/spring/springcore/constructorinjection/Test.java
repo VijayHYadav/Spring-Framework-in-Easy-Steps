74c02f12470240b31df191d8072b1a4b62358fe9
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-10 18:47:57 +0530

    67. Create and run the Test

---
+++ springcore/src/main/java/com/bharath/spring/springcore/constructorinjection/Test.java
@@ -0,0 +1,12 @@
+package com.bharath.spring.springcore.constructorinjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("constructorinjection.xml");
+        Employee employee = (Employee) context.getBean("employee");
+        System.out.println(employee);
+    }
+}
