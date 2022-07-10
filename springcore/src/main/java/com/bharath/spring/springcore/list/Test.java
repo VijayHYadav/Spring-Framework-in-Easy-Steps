0daaf88527c6f4554b49256676cb81d642a23f74
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-10 06:16:39 +0530

    35. List - Create the Test , 36. Running the test and flow

---
+++ springcore/src/main/java/com/bharath/spring/springcore/list/Test.java
@@ -0,0 +1,13 @@
+package com.bharath.spring.springcore.list;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("listconfig.xml");
+        Hospital hospital = (Hospital) context.getBean("hospital");
+        System.out.println(hospital.getName());
+        System.out.println(hospital.getDepartments());
+    }
+}
