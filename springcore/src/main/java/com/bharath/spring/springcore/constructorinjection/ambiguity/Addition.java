36fb38bf2b3c70d8b2a20f8eb895267f32175bfd
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-10 19:44:42 +0530

    71. Ambiguity Problem Variation

--- springcore/src/main/java/com/bharath/spring/springcore/constructorinjection/ambiguity/Addition.java
+++ springcore/src/main/java/com/bharath/spring/springcore/constructorinjection/ambiguity/Addition.java
@@ -3,14 +3,8 @@ package com.bharath.spring.springcore.constructorinjection.ambiguity;
 public class Addition {
 
 
-    Addition(double a,double b) {
-        System.out.println("Inside Constructor Double");
-    }
-    Addition(int a,int b) {
-        System.out.println("Inside Constructor INT");
+    Addition(int a,double b) {
+        System.out.println("Inside Constructor");
     }
 
-    Addition(String a, String b) {
-        System.out.println("Inside Constructor String");
-    }
 }
