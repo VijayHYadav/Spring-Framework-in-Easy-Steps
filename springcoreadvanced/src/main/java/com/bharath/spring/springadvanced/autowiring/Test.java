e74b0612b8b9c310d08344fb9347bc771118ab00
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-11 08:51:11 +0530

    78. Create a Maven Project, 79. Auto-Wiring by type

---
+++ springcoreadvanced/src/main/java/com/bharath/spring/springadvanced/autowiring/Test.java
@@ -0,0 +1,12 @@
+package com.bharath.spring.springadvanced.autowiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Employee employee = (Employee) context.getBean("employee");
+        System.out.println(employee);
+    }
+}
