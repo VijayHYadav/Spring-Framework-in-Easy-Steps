3dc126d3eb58d9f1f14908e32e925d9fad041084
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-13 07:54:08 +0530

    117. Implement the create method

--- springjdbc/src/main/java/com/bharath/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
+++ springjdbc/src/main/java/com/bharath/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
@@ -2,10 +2,24 @@ package com.bharath.spring.springjdbc.employee.dao.impl;
 
 import com.bharath.spring.springjdbc.employee.dao.EmployeeDao;
 import com.bharath.spring.springjdbc.employee.dto.Employee;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class EmployeeDaoImpl implements EmployeeDao {
+
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public int create(Employee employee) {
-        return 0;
+        String sql = "insert into employee values(?,?,?)";
+        int result = jdbcTemplate.update(sql, employee.getId(), employee.getFirtname(), employee.getLastname());
+        return result;
+    }
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
