7238f74b8a8d00c9d55d36329c49a534050780f2
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-13 07:34:07 +0530

    114. Use the JDBCTemplate to perform Insert operation

---
+++ springjdbc/src/main/java/com/bharath/spring/springjdbc/Test.java
@@ -0,0 +1,15 @@
+package com.bharath.spring.springjdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Test {
+    public static  void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+        JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+        String sql = "insert into employee values(?,?,?)";
+        int result = jdbcTemplate.update(sql, 1,"Vijay", "Yadav");
+        System.out.println("Number of result : "+result);
+    }
+}
