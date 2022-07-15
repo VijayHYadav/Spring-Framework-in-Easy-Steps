d8766c6ec80706c97445dad6667ad0fbcd852170
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-15 23:44:43 +0530

    140. Configure and use the Hibernate Template

--- springorm/src/main/java/com/bharath/spring/springorm/product/dao/impl/ProductDaoImpl.java
+++ springorm/src/main/java/com/bharath/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -2,10 +2,14 @@ package com.bharath.spring.springorm.product.dao.impl;
 
 import com.bharath.spring.springorm.product.dao.ProductDao;
 import com.bharath.spring.springorm.product.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductDaoImpl implements ProductDao {
 
+    @Autowired
     HibernateTemplate hibernateTemplate;
 
     @Override
