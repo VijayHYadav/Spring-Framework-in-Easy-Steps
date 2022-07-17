36c797774bc3d7c35e77293ccece95cf82fd5cdf
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-17 09:45:01 +0530

    172. Create the second controller method

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/UserController.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.bharath.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.bharath.spring.springmvc.dto.User;
 
 @Controller
 public class UserController {
 
 	@RequestMapping("registrationPage")
 	public ModelAndView showRegistrationPage() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("userReg");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
+	public ModelAndView registerUser(@ModelAttribute("user")User user) {
+		System.out.println(user);
+		
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("userReg");
 		return modelAndView;
