1741eaff59873bbfdf836ca1497065d89cd803b2
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-21 07:02:44 +0530

    220. Create the advices

--- springaop/src/main/java/com/bharat/spring/springaop/aspect/LogginAspect.java
+++ springaop/src/main/java/com/bharat/spring/springaop/aspect/LogginAspect.java
@@ -1,5 +1,20 @@
 package com.bharat.spring.springaop.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
 public class LogginAspect {
 
+	@Before("")
+	public void logBefore(JoinPoint joinpoint) {
+		System.out.println("Before Calling the method");
+	}
+	
+	@After("")
+	public void logAfter(JoinPoint joinPoint) {
+		System.out.println("After the method execution");
+	}
 }
