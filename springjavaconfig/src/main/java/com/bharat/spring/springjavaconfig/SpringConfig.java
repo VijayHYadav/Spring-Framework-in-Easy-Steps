4ad0a39c116d64c47dce34ca2e839ef3360c1366
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-22 07:03:05 +0530

    234. Assignment - Use @Scope.

--- springjavaconfig/src/main/java/com/bharat/spring/springjavaconfig/SpringConfig.java
+++ springjavaconfig/src/main/java/com/bharat/spring/springjavaconfig/SpringConfig.java
@@ -3,6 +3,7 @@ package com.bharat.spring.springjavaconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @Import(DaoConfig.class)
@@ -14,6 +15,8 @@ public class SpringConfig {
 //	}
 	
 	@Bean(initMethod = "init", destroyMethod = "destroy")
+//	@Scope("prototype")
+//	@Scope("singleton")
 	public Service service() {
 		return new Service();
 	}
