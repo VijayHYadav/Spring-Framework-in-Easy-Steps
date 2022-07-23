73b7c33510d484b0efd8b35ca9a458d287e66fa0
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-23 08:34:54 +0530

    240. Configure Resource View Resolver

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
@@ -1,5 +1,23 @@
 package com.bharath.spring.springmvc.controller;
 
-public class SpringConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
+@ComponentScan("com.bharath.spring.springmvc.controller")
+@Configuration
+public class SpringConfig extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
 }
