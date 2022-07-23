25a82e76f5a375fd645febc6dd75461089dc0499
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-23 09:14:21 +0530

    243. Configure Dispatcher Servlet Dynamically

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/WebServletConfiguration.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/WebServletConfiguration.java
@@ -2,17 +2,22 @@ package com.bharath.spring.springmvc.controller;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebServletConfiguration implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		// TODO Auto-generated method stub
 		AnnotationConfigApplicationContext webContext = new AnnotationConfigApplicationContext();
 		webContext.register(SpringConfig.class);
+		
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet());
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
 	}
 
 }
