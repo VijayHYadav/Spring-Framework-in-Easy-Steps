cd7dca8b03422a8a98796016dd476b596854def6
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-24 05:25:14 +0530

    263. Create

--- springdatajpa/src/test/java/com/bharath/spring/data/springdatajpa/SpringdatajpaApplicationTests.java
+++ springdatajpa/src/test/java/com/bharath/spring/data/springdatajpa/SpringdatajpaApplicationTests.java
@@ -1,13 +1,29 @@
 package com.bharath.spring.data.springdatajpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.bharath.spring.data.springdatajpa.data.entities.Product;
+import com.bharath.spring.data.springdatajpa.repos.ProductRepository;
 
 @SpringBootTest
 class SpringdatajpaApplicationTests {
 
+	@Autowired
+	ApplicationContext context;
+	
 	@Test
-	void contextLoads() {
+	public void saveProduct() {
+		ProductRepository repository = context.getBean(ProductRepository.class);
+		
+		Product product = new Product();
+		product.setId(10L);
+		product.setName("keyboard");
+		product.setDescription("this is world class keyboard.");
+		product.setPrice(1000d);
+		repository.save(product);
 	}
 
 }
