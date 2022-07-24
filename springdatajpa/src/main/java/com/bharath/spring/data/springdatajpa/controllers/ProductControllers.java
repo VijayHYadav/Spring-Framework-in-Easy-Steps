94139c754589dec7187de89b96cc7b81c454e1ec
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-24 08:48:49 +0530

    284. Update Product, 285. READ Product by ID

--- springdatajpa/src/main/java/com/bharath/spring/data/springdatajpa/controllers/ProductControllers.java
+++ springdatajpa/src/main/java/com/bharath/spring/data/springdatajpa/controllers/ProductControllers.java
@@ -1,8 +1,12 @@
 package com.bharath.spring.data.springdatajpa.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +36,20 @@ public class ProductControllers {
 	@PostMapping
 	public Product create(@RequestBody Product product) {
 		return repository.save(product);
+	}
+	
+	/**
+	 * repository.save. This save method internally, 
+	 * it knows whether a particular product is already there.
+	 * If it's there it will update that information instead of creating it by looking at the ID field in the
+	 * request and it returns that product back. Stop */
+	@PutMapping
+	public Product update(@RequestBody Product product) {
+		return repository.save(product);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Product> getProduct(@PathVariable("id") Long id) {
+		return repository.findById(id);
 	}
 }
