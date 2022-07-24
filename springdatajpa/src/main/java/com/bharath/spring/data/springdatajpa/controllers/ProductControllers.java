b7f900da0c89bdcd9d2b5093b9d829e0c015762a
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-24 08:24:47 +0530

    282. Implement Create

--- springdatajpa/src/main/java/com/bharath/spring/data/springdatajpa/controllers/ProductControllers.java
+++ springdatajpa/src/main/java/com/bharath/spring/data/springdatajpa/controllers/ProductControllers.java
@@ -2,6 +2,8 @@ package com.bharath.spring.data.springdatajpa.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,10 @@ public class ProductControllers {
 	@GetMapping
 	public Iterable<Product> getProducts() {
 		return repository.findAll();
+	}
+
+	@PostMapping
+	public Product create(@RequestBody Product product) {
+		return repository.save(product);
 	}
 }
